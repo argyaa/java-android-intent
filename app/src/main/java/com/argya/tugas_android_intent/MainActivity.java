10c70222ac57990b124e4c78780af8c1687ec21b
Argya Aulia Fauzandika <72845615+argyaa@users.noreply.github.com>
2022-04-22 08:05:54 +0700

    Update MainActivity.java

--- app/src/main/java/com/argya/tugas_android_intent/MainActivity.java
+++ app/src/main/java/com/argya/tugas_android_intent/MainActivity.java
@@ -1,3 +1,9 @@
+// Nama : Argya Aulia Fauzandika
+// NIM  : 10119035
+// Kelas: IF-1
+// Tanggal Pengerjaan : 22 April 2022
+
+
 package com.argya.tugas_android_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
