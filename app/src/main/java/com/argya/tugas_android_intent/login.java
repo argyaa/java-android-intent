37558de5ab3414bfff817436b7301ef87ca02ac6
Argya Aulia Fauzandika <72845615+argyaa@users.noreply.github.com>
2022-04-22 08:07:27 +0700

    Update login.java

--- app/src/main/java/com/argya/tugas_android_intent/login.java
+++ app/src/main/java/com/argya/tugas_android_intent/login.java
@@ -1,3 +1,8 @@
+// Nama : Argya Aulia Fauzandika
+// NIM  : 10119035
+// Kelas: IF-1
+// Tanggal Pengerjaan : 22 April 2022
+
 package com.argya.tugas_android_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
